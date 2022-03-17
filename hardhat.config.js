3fc18178f758339613522ba71f274392a0160ab6
Facundo <facundo.bo@hotmail.com>
2022-03-17 14:05:44 -0300

    primer commit

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const myKey = "175ccc91b14c2fc4a557f34c445eb9e67aa92ed4fd20816ef4d502295e30bf82"
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/IWGv4X1GXHgV5LzUNEIzxkc5OicKf3C1',
+      accounts: [`${myKey}`]
+    }
+  }
+};
