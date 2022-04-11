3ccbad427b46354e38a0072a5dc6e17300bc27f7
Facundo <facundo.bo@hotmail.com>
2022-04-11 13:15:21 -0300

    commit save

--- src/components/pages/imageHandler.js
+++ src/components/pages/imageHandler.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './imageHandler.css';
+import Form from 'react-bootstrap/Form'
+import Moralis from "moralis"
+
 
 export class ImageHandler extends Component {
   state={
@@ -16,26 +19,55 @@ export class ImageHandler extends Component {
     reader.readAsDataURL(e.target.files[0])
   };
 	render() {
-    const { profileImg} = this.state
+    const { profileImg} = this.state;
+
+    const myOnClick = () => {
+      alert("prueba")
+    }
+
+    async function onChange(e) {
+      setLoadingImg(true)
+      const file = e.target.files[0]
+      try {
+          console.log("el loadingImg: ",loadingImg)
+          console.log("el file: ",file)
+          const filee = new Moralis.File(file.name, file)
+          await filee.saveIPFS({useMasterKey: true});
+          console.log(filee.ipfs(), filee.hash())
+          const url = filee.ipfs()
+          updateFileUrl(url)
+          console.log('el file URL: ' + fileUrl)
+          setLoadingImg(false)
+      } catch (error) {
+          console.log('Error uploading file: ', error)
+      }
+    }
+
 		return (
-			<div className="page">
-				<div className="container">
-					<h1 className="heading">Add your Image</h1>
-                    <input type="file" accept="image/*" name="image-upload" id="input" onChange={this.imageHandler} />
-					<div className="img-holder" onClick={this.imageHandler} style={{
-                        borderStyle: 'dotted',
-                        borderWidth: 5,
-                        borderRadius: 7,
-                    }}>
-						<img src={profileImg} alt="" id="img" className="img" />
-					</div>
-					<div className="label">
-                    <label className="image-upload" htmlFor="input">
-						<i className="material-icons">add_photo_alternate</i>
-						Choose your Photo
-					</label>
+			<div className="page" onClick={myOnClick}>
+          <div className="container">
+        <a href="">
+            <h1 className="heading">Add your Image</h1>
+            {/* <input type="file" accept="image/*" name="image-upload" id="input" onChange={this.imageHandler} /> */}
+            <Form.Group controlId="formFileLg" className="mb-3">
+                    <Form.Label>Large file input example</Form.Label>
+                    <Form.Control onChange={onChange} type="file"/>
+            <div className="img-holder" onClick={this.imageHandler} style={{
+              borderStyle: 'dotted',
+              borderWidth: 5,
+              borderRadius: 7,
+            }}>
+              <img src={profileImg} alt="" id="img" className="img" />
+            </div>
+                        </Form.Group>
+            <div className="label">
+                      <label className="image-upload" htmlFor="input">
+              <i className="material-icons">add_photo_alternate</i>
+              Choose your Photo
+            </label>
+            </div>
+        </a>
           </div>
-				</div>
 			</div>
 		);
 	}
