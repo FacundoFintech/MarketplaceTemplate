e6321538e36e584b34d5469af186616ff0975372
Facundo <facundo.bo@hotmail.com>
2022-04-13 13:48:33 -0300

    skeleton add

--- src/components/components/ImageGallery/index.js
+++ src/components/components/ImageGallery/index.js
@@ -4,6 +4,7 @@ import './ImageGallery.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Placeholder from 'react-bootstrap/Placeholder';
 
 const ImageGallery = () => {
 
@@ -19,36 +20,45 @@ const ImageGallery = () => {
   }, [isLoading, user/* ,address , user.attribues.ethAddress , user*/]);
 
 
-  if (isLoading) {
-    return <h1>Loading... </h1>
-  } else {
-    return (
+  // if (isLoading) {
+    return ( 
       <>
-        <div className="justify-content-center">
-          <div className="p-4">
-            <Container>
-              <Row>
-                {data?.result.filter(nft=>nft.image).map((nft, index) => {
-                  return (
-                    <Col xs={6} md={4}>
-                      <div key={index} className="nft-container">
-                        <img src={nft.image} alt="foto-nft" className="gallery-foto overflow-hidden" />
-                        <div className="p-1 bg-dark name-container">
-                          <p className="nft-name">{nft.name}</p>
-                        </div>
-                      </div>
-                    </Col>
-                  )
-                }
-
-                )}
-              </Row>
-            </Container>
-          </div>
-        </div>
+        <Placeholder as="p" animation="glow">
+          <Placeholder xs={12} />
+        </Placeholder>
+        <Placeholder as="p" animation="wave">
+          <Placeholder xs={12} />
+        </Placeholder> 
       </>
-    )
-  }
+  )
+  // } else {
+  //   return (
+  //     <>
+  //       <div className="justify-content-center">
+  //         <div className="p-4">
+  //           <Container>
+  //             <Row>
+  //               {data?.result.filter(nft=>nft.image).map((nft, index) => {
+  //                 return (
+  //                   <Col xs={6} md={4}>
+  //                     <div key={index} className="nft-container">
+  //                       <img src={nft.image} alt="foto-nft" className="gallery-foto overflow-hidden" />
+  //                       <div className="p-1 bg-dark name-container">
+  //                         <p className="nft-name">{nft.name}</p>
+  //                       </div>
+  //                     </div>
+  //                   </Col>
+  //                 )
+  //               }
+
+  //               )}
+  //             </Row>
+  //           </Container>
+  //         </div>
+  //       </div>
+  //     </>
+  //   )
+  // }
 }
 
 
